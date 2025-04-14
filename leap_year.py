65503a79d1cdd38cac71b4162936ec9458e4f2aa
FranciscoM12 <chamfle8@gmail.com>
2025-04-14 12:41:39 -0300

    entrega tp

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,7 @@
 def leap_year():
-    print("TO DO")
+    year=input("Ingrese un año: ")
+    year=int(year)
+    if year % 4 == 0 and  not year %100 ==0 or year % 400 == 0:
+        print(f"El año {year} es bisiesto")
+    else:
+        print(f"El año {year} no es bisiesto")
